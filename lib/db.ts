8c9c4ef387b866569b1e8937a765a9a25e853b2b
POBOROJO <parijatbhatt1@gmail.com>
2025-02-10 03:32:59 +0530

    add validation schemas for login and registration, update database connection logic, and enhance user model

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-  try{
-    await mongoose.connect(process.env.MONGO_URI!);
-    console.log("MongoDB connected");
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  // Check if we have a connection to the database or if it's currently connecting
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
   }
-  catch(err){
-    console.error("Error",err);
+
+  try {
+    // Attempt to connect to the database
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+
+    // Graceful exit in case of a connection error
     process.exit(1);
   }
 }
 
-export default connectDB;
+export default dbConnect;
--- model/User.ts
+++ model/User.ts
@@ -1,26 +1,31 @@
-import mongoose,{Document,Schema} from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-interface IUser extends Document{
-    name: string;
-    email: string;
-    password: string;
-    role: string;
-    createdAt: Date
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
 }
 
-const UserSchema = new Schema({
-    name: { type: String, require: true },
-  email: { type: String, require: true },
-  password: { type: String, require: true },
+const UserSchema: Schema<IUser> = new Schema({
+  name: { type: String, require: [true, "Name is required"], trim: true },
+  email: {
+    type: String,
+    require: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: { type: String, require: [true, "Password is required"] },
   role: {
     type: String,
     enum: ["user", "admin"],
     default: "user",
     require: true,
   },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now, require: true },
 });
 
-const User = mongoose.models?.User || mongoose.model<IUser>("User", UserSchema);
+const UserModel = (mongoose.models.User as mongoose.Model<IUser>) || mongoose.model<IUser>("User", UserSchema);
 
-export default User;
+export default UserModel;
